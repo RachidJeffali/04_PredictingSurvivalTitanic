3a18f017bdbac58f6843597279108b7b7c74104e
Rachid Jeffali <rachid.jeffali@gmail.com>
2018-11-02 20:58:52 +0300

    uodate 20:58

--- titanicPrediction.py
+++ titanicPrediction.py
@@ -95,9 +95,25 @@ train_df['not_alone'].value_counts()
 
 axes = sns.factorplot('relatives', 'Survived', data = train_df, aspect = 2.5)
 
+# data preprocessing
+train_df = train_df.drop(['PassengerId'], axis=1)
 
+# fix Cabin issue
+import re
+deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
+data = [train_df, test_df]
 
+for dataset in data:
+    dataset['Cabin'] = dataset['Cabin'].fillna("U0")
+    dataset['Deck'] = dataset['Cabin'].map(lambda x : re.compile("([a-zA-Z]+)").search(x).group())
+    dataset['Deck'] = dataset['Deck'].map(deck)
+    dataset['Deck'] = dataset['Deck'].fillna(0)
+    dataset['Deck'] = dataset['Deck'].astype(int)
+    
+train_df = train_df.drop(['Cabin'], axis = 1)
+test_df = test_df.drop(['Cabin'], axis = 1)
 
+# Fix Age issue
 
 
 
